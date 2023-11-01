ebd5a5bc9a276436f3c70bbe83da8b6cae5ac029
vinothini <knvinothini@gmail.com>
2023-11-01 13:59:16 -0400

    rename

--- src/main/java/com/student/studentregister/StudentDisplayController.java
+++ src/main/java/com/student/studentregister/StudentDisplayController.java
@@ -3,7 +3,7 @@ package com.student.studentregister;
 import java.io.IOException;
 import javafx.fxml.FXML;
 
-public class SecondaryController {
+public class StudentDisplayController {
 
     @FXML
     private void switchToPrimary() throws IOException {
--- src/main/java/com/student/studentregister/StudentRegisterController.java
+++ src/main/java/com/student/studentregister/StudentRegisterController.java
@@ -5,7 +5,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Label;
 
-public class StudentController implements StudentInterface{
+public class StudentRegisterController implements StudentInterface{
 
     @FXML
     private TextField firstNameField;
